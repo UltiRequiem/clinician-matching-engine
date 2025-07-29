4fb1efe82db11eec0d73f0f991b52ece056814d7
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-07-28 19:04:12 -0500

    feat: states selector

---
+++ app/web/src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const STATES = [
+    "California",
+    "Texas",
+    "Florida",
+    "New York",
+    "Illinois",
+    "Pennsylvania",
+    "Ohio",
+    "Georgia",
+    "North Carolina",
+    "Michigan",
+];
